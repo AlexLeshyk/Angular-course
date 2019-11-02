264701cbdab61022d6bd0803814bce4217d4e66e
AlexLeshyk <gashy_jet@yahoo.com>
2019-11-02 16:43:39 +0300

    Change some test to success

--- src/app/course-page/components/course-page/course-page.component.spec.ts
+++ src/app/course-page/components/course-page/course-page.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CoursePageComponent } from './course-page.component';
+import { CourseItemComponent } from '../course-item/course-item.component';
 
 describe('CoursePageComponent', () => {
   let component: CoursePageComponent;
@@ -8,7 +9,7 @@ describe('CoursePageComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ CoursePageComponent ]
+      declarations: [ CoursePageComponent, CourseItemComponent ]
     })
     .compileComponents();
   }));
