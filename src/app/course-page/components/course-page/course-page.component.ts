626d13a3c205e2cd29f934e75bbb1734b7a770da
AlexLeshyk <gashy_jet@yahoo.com>
2019-11-21 01:00:26 +0300

    Add service

--- src/app/course-page/components/course-page/course-page.component.ts
+++ src/app/course-page/components/course-page/course-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, SimpleChanges } from '@angular/core';
 import { CourseItem } from '../../models/course-item.model';
+import { ItemCourseService } from '../../services/item-course.service'
 
 @Component({
   selector: 'app-course-page',
@@ -10,62 +11,13 @@ export class CoursePageComponent implements OnInit {
 
   inputValue = 'course';
 
-  public courseItems: Array<CourseItem> = [
-    {
-      id: 1,
-      title: 'Video converting course',
-      description: 'Learn about where you can find course descriptions, what information they include, how they work, and details about various components of a course description. Course descriptions report information about a university or college\'s classes. They\'re published both in course catalogs that outline degree requirements and in course schedules that contain descriptions for all courses offered during a particular semester.',
-      duration: 130,
-      dateObj: Date.parse("January 26, 2020"),
-      topRated: false
-    },
-    {
-      id: 2,
-      title: 'React in other life course',
-      description: 'some text here',
-      duration: 40,
-      dateObj: Date.parse("Nov 12, 2019"),
-      topRated: false
-    },
-    {
-      id: 3,
-      title: 'Awesome course',
-      description: 'fdf gsdsf dgfdfgdf dhdghrqertqrt tretw',
-      duration: 95,
-      dateObj: Date.parse("Feb 20, 2020"),
-      topRated: false
-    },
-    {
-      id: 4,
-      title: 'Angular course',
-      description: 'fggdfg dgfdfg gdfgd lorem',
-      duration: 56,
-      dateObj: Date.parse("Nov 3, 2019"),
-      topRated: false
-    },
-    {
-      id: 5,
-      title: 'Javascript language course',
-      description: 'fggdfg dgfdfg gdfgd lorem',
-      duration: 67,
-      dateObj: Date.parse("Nov 11, 2019"),
-      topRated: true
-    },
-    {
-      id: 6,
-      title: 'Some more else course',
-      description: 'fggdfg dgfdfg gdfgd lorem',
-      duration: 202,
-      dateObj: Date.parse("Oct 30, 2019"),
-      topRated: true
-    }
-  ]
+  courseItems: CourseItem[];
 
   public counter: number = 0;
   public isShowCourse: boolean = false;
 
   public ngOnChanges(changes: SimpleChanges): void {
-    console.log('OnChanges CoursePage Component', changes);
+    // console.log('OnChanges CoursePage Component', changes);
   }
 
   public change(): void {
@@ -74,10 +26,14 @@ export class CoursePageComponent implements OnInit {
     this.isShowCourse = this.isShowCourse ? false : true;
   }
 
-  constructor() { }
+  constructor(private itemCourseService: ItemCourseService) { }
+
+  getItems(): void {
+    this.itemCourseService.getItems().subscribe(items => this.courseItems = items);
+  }
 
   ngOnInit() {
-    console.log('OnInit CoursePage Component', this.counter);
+    this.getItems();
   }
 
   public onItemDelete(item: CourseItem): void {
---
+++ src/app/course-page/services/item-course.service.ts
@@ -0,0 +1,65 @@
+import { Injectable } from '@angular/core';
+import { CourseItem } from '../models/course-item.model';
+import { Observable, of } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ItemCourseService {
+
+  public items: Array<CourseItem> = [
+    {
+      id: 1,
+      title: 'Video converting course',
+      description: 'Learn about where you can find course descriptions, what information they include, how they work, and details about various components of a course description. Course descriptions report information about a university or college\'s classes. They\'re published both in course catalogs that outline degree requirements and in course schedules that contain descriptions for all courses offered during a particular semester.',
+      duration: 130,
+      dateObj: Date.parse("January 26, 2020"),
+      topRated: false
+    },
+    {
+      id: 2,
+      title: 'React in other life course',
+      description: 'some text here',
+      duration: 40,
+      dateObj: Date.parse("Nov 12, 2019"),
+      topRated: false
+    },
+    {
+      id: 3,
+      title: 'Awesome course', description: 'fdf gsdsf dgfdfgdf dhdghrqertqrt tretw',
+      duration: 95,
+      dateObj: Date.parse("Feb 20, 2020"),
+      topRated: false
+    },
+    {
+      id: 4,
+      title: 'Angular course',
+      description: 'fggdfg dgfdfg gdfgd lorem',
+      duration: 56,
+      dateObj: Date.parse("Nov 3, 2019"),
+      topRated: false
+    },
+    {
+      id: 5,
+      title: 'Javascript language course',
+      description: 'fggdfg dgfdfg gdfgd lorem',
+      duration: 67,
+      dateObj: Date.parse("Nov 11, 2019"),
+      topRated: true
+    },
+    {
+      id: 6,
+      title: 'Some more else course',
+      description: 'fggdfg dgfdfg gdfgd lorem',
+      duration: 202,
+      dateObj: Date.parse("Oct 30, 2019"),
+      topRated: true
+    }
+  ]
+
+  constructor() { }
+
+  public getItems(): Observable<CourseItem[]> {
+    return of(this.items);
+  }
+}
